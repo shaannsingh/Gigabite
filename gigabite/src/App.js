e042ceaed6a340b392c66e90c27721a397ee97c5
Shaan Singh <ss5432@outlook.com>
2024-08-01 11:07:39 -0400

    First change ever!

--- gigabite/src/App.js
+++ gigabite/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Recipes from './components/Recipes';
 
 function App() {
   return (
     <div className="App">
-      <h1>AI Cookbook</h1>
+      <h1>Gigabite</h1>
+      <Recipes />
     </div>
   );
 }
---
+++ gigabite/src/components/Recipes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Recipes() {
+    return (
+        <div>
+            <h2>Recipes!</h2>
+            Onion Soup
+            Goat Cock
+        </div>
+    );
+}
+
+export default Recipes;
